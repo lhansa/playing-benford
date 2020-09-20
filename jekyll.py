81abcb90603e496467be6e0aaf0311e6886f7e1d
Leonardo Hansa <hansa.leonardo@gmail.com>
2020-09-20 20:31:32 +0200

    mas pruebas twitter

--- jekyll.py
+++ jekyll.py
@@ -1,13 +1,24 @@
-import tweepy
+#%% Librerías
+import tweepy           
 import matplotlib.pyplot as plt
 import time
+import pprint
 
-# Autenticación
+#%% Autenticación
 exec(open('acceso.py').read())
 
-usuario = api.get_user('PabloIglesias')
+# usuario = api.get_user('PabloIglesias')
 
-l_followers = tweepy.Cursor(api.followers, screen_name='l_hansa').items(5)
+#%% Descarga followers
+l_followers = tweepy.Cursor(api.followers, screen_name='l_hansa').items()
+
+#%% Explora followers
+count = 0
+for follower in l_followers:   
+    count+=1
+
+print(count)
+
 
 n_followers = [c.followers_count for c in l_followers]
 n_followers = []
