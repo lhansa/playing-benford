9a4ee09aaf8f98c91c9418ca77f87296bc1fb955
Leonardo Hansa <hansa.leonardo@gmail.com>
2020-09-20 13:17:41 +0200

    Sigo intentando Twitter

--- jekyll.py
+++ jekyll.py
@@ -7,6 +7,12 @@ exec(open('acceso.py').read())
 
 usuario = api.get_user('PabloIglesias')
 
-l_followers = tweepy.Cursor(api.followers, screen_name='l_hansa')
-         
-l_followers
+l_followers = tweepy.Cursor(api.followers, screen_name='l_hansa').items(5)
+
+n_followers = [c.followers_count for c in l_followers]
+n_followers = []
+for c in l_followers:
+    n_followers.append(c.followers_count)
+    time.sleep(3)
+
+print(n_followers)
