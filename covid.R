f7df7a9167b922dd344c593f79334b211a663659
Leonardo Hansa <hansa.leonardo@gmail.com>
2020-09-20 19:38:42 +0200

    No acumulado covid

--- covid.R
+++ covid.R
@@ -18,7 +18,7 @@ df_benford <- tibble(
   benford = log10(1 + 1/1:9) 
 )
  
-
+# Contagiados acumulados
 df_covid_benford %>% 
   ggplot(aes(x = primero)) + 
   geom_bar(aes(y = ..prop..)) +
@@ -32,6 +32,51 @@ df_covid_benford %>%
   labs(
     title = "Primer dígito del número de casos de Covid", 
     subtitle = "Datos diarios, por CCAA, desde el 21 de febrero", 
+    x = "Primer dígito", y = "Proporción", 
+    caption = "Fuente (datos): Datadista."
+  )
+
+# Contagiados no acumulados
+df_covid_nocum <- df_covid_benford %>% 
+  group_by(cod_ine, CCAA) %>% 
+  mutate(value = value - lag(value)) %>% 
+  ungroup() %>% 
+  filter(!is.na(value), value > 0) %>% 
+  mutate(primero = calcula_primero(value))
+
+df_covid_nocum %>% 
+  ggplot(aes(x = primero)) + 
+  geom_bar(aes(y = ..prop..)) +
+  geom_line(data = df_benford, aes(x = eje_x, y = benford), 
+            col = "red", size = 1) + 
+  geom_text(aes(y = ..prop.., 
+                label = scales::percent(..prop..)), 
+            stat= "count",
+            vjust = -.5) + 
+  scale_x_continuous(breaks = 1:9) + 
+  labs(
+    title = "Primer dígito del número de casosde Covid", 
+    subtitle = "Datos diarios (no cum), por CCAA, de febrero a mayo", 
+    x = "Primer dígito", y = "Proporción", 
+    caption = "Fuente (datos): Datadista."
+  )
+
+## Contagiados no cum, por CCCAA
+
+df_covid_nocum %>% 
+  ggplot(aes(x = primero)) + 
+  geom_bar(aes(y = ..prop..)) +
+  geom_line(data = df_benford, aes(x = eje_x, y = benford), 
+            col = "red", size = 1) + 
+  geom_text(aes(y = ..prop.., 
+                label = scales::percent(..prop..)), 
+            stat= "count",
+            vjust = -.5) + 
+  facet_wrap(~ CCAA) + 
+  scale_x_continuous(breaks = 1:9) + 
+  labs(
+    title = "Primer dígito del número de casosde Covid", 
+    subtitle = "Datos diarios (no cum), por CCAA, de febrero a mayo", 
     x = "Primer dígito", y = "Proporción", 
     caption = "Fuente (datos): Datadista."
   )
